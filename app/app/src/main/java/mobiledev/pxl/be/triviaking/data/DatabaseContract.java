46ad7ccecd8ca66149673c37c1b2082a2cbc3673
DarySchaeken <dary.schaeken@student.pxl.be>
2018-10-31 15:34:49 +0100

    Basic datalayer

---
+++ app/app/src/main/java/mobiledev/pxl/be/triviaking/data/DatabaseContract.java
@@ -0,0 +1,14 @@
+package mobiledev.pxl.be.triviaking.data;
+
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+    private DatabaseContract(){}
+
+    public static class Quiz implements BaseColumns {
+        public static final String TABLE_NAME = "quiz";
+        public static final String CREATED =  "created";
+        public static final String CATEGORY = "category";
+        public static final String DATA = "data";
+    }
+}
---
+++ app/app/src/main/java/mobiledev/pxl/be/triviaking/data/DbHelper.java
@@ -0,0 +1,32 @@
+package mobiledev.pxl.be.triviaking.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "TriviaKing.db";
+
+    private static final int DATABASE_VERSION = 0;
+
+    public DbHelper(Context context) {
+        super(context,DATABASE_NAME,null,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_QUIZ_TABLE = "CREATE TABLE " + DatabaseContract.Quiz.TABLE_NAME + " (" +
+                DatabaseContract.Quiz._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                DatabaseContract.Quiz.CREATED + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP," +
+                DatabaseContract.Quiz.CATEGORY + " TEXT NOT NULL," +
+                DatabaseContract.Quiz.DATA + " TEXT NOT NULL"+
+                ")";
+        db.execSQL(SQL_CREATE_QUIZ_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + DatabaseContract.Quiz.TABLE_NAME);
+        onCreate(db);
+    }
+}
