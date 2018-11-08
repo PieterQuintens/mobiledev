88c844e21df9ab2ea0d6503fac6c017458508380
PieterQuintens <pieter.quintens@hotmail.com>
2018-11-08 13:37:28 +0100

    Updated QrCodeSupporter

--- app/app/src/main/java/mobiledev/pxl/be/triviaking/support/QRCodeSupporter.java
+++ app/app/src/main/java/mobiledev/pxl/be/triviaking/support/QRCodeSupporter.java
@@ -1,13 +1,22 @@
 package mobiledev.pxl.be.triviaking.support;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapRegionDecoder;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.widget.ImageView;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import mobiledev.pxl.be.triviaking.R;
 
 import static android.graphics.Color.BLACK;
 import static android.graphics.Color.WHITE;
@@ -15,10 +24,14 @@ import static android.graphics.Color.WHITE;
 public class QRCodeSupporter {
     public static Bitmap encodeAsBitmap(String content, ImageView resultLocation) throws WriterException {
         BitMatrix result;
+
+        Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
+        hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+
         int width = resultLocation.getWidth();
         try {
             result = new MultiFormatWriter().encode(content,
-                    BarcodeFormat.QR_CODE, width, width, null);
+                    BarcodeFormat.QR_CODE, width, width, hintMap);
         } catch (IllegalArgumentException iae) {
             return null;
         }
@@ -33,7 +46,28 @@ public class QRCodeSupporter {
         }
         Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         bitmap.setPixels(pixels, 0, width, 0, 0, w, h);
-        resultLocation.setImageBitmap(bitmap);
+
+        Bitmap overlay = BitmapFactory.decodeResource(resultLocation.getResources(), R.mipmap.ic_launcher);
+        resultLocation.setImageBitmap(mergeBitmaps(overlay, bitmap));
+//        resultLocation.setImageBitmap(bitmap);
         return bitmap;
+    }
+
+    public static Bitmap mergeBitmaps(Bitmap overlay, Bitmap bitmap) {
+        int height = bitmap.getHeight();
+        int width = bitmap.getWidth();
+
+        Bitmap combined = Bitmap.createBitmap(width, height, bitmap.getConfig());
+        Canvas canvas = new Canvas(combined);
+        int canvasWidht = canvas.getWidth();
+        int canvasHeight = canvas.getHeight();
+
+        canvas.drawBitmap(bitmap, new Matrix(), null);
+
+        int centreX = (canvasWidht - overlay.getWidth()) / 2;
+        int centreY = (canvasHeight - overlay.getHeight()) / 2;
+        canvas.drawBitmap(overlay, centreX, centreY, null);
+
+        return combined;
     }
 }
